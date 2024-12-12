23bd7e589b50ee624eed1c218784bd2da521ba0d
Saied Kazemi <saied.kazemi@lip6.fr>
2024-12-12 13:05:23 +0100

    Fix the script to scan containers other than the default

--- tools/scan_logs.sh
+++ tools/scan_logs.sh
@@ -33,36 +33,41 @@ main() {
 }
 
 scan_logs() {
-        local start_date="${START_DATE}"
-        local end_date="${END_DATE}"
-        local tmpfile
-        local addr
-        local i
-        local next_start_date
-        local to_date
+	local start_date
+	local end_date
+	local tmpfile
+	local addr
+	local i
+	local next_start_date
+	local to_date
 
-        if [[ "${START_DATE}" != *:* ]]; then
-                start_date="${START_DATE}T00:00:00Z"
-        fi
-        if [[ "${END_DATE}" == "" ]]; then
-                END_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
-        elif [[ "${END_DATE}" != *:* ]]; then
-                end_date="${END_DATE}T00:00:00Z"
-        fi
-        tmpfile="$(mktemp /tmp/worker_logs.$$.XXXX)"
-        addr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' iris_loki_1)
-        i=1
-        while [[ ! "${start_date}" > "${end_date}" ]]; do
-                next_start_date=$(add_days "${start_date}" 30)
-                to_date="$(subtract_second "${next_start_date}")"
-                (1>&2 date)
-                (1>&2 echo "logcli --addr=http://${addr}:3100 query ${QUERY} --from=${start_date} --to=${to_date} --limit 200000000 > ${tmpfile}.${i}")
-                logcli --addr="http://${addr}:3100" query "${QUERY}" --from="${start_date}" --to="${to_date}" --limit 200000000 > "${tmpfile}.${i}"
-                grep --color -i "${PATTERN}" "${tmpfile}.${i}" || :
+	if [[ "${START_DATE}" != *:* ]]; then
+		start_date="${START_DATE}T00:00:00Z"
+	else
+		start_date="${START_DATE}"
+	fi
+	if [[ "${END_DATE}" == "" ]]; then
+		END_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+	fi
+	if [[ "${END_DATE}" != *:* ]]; then
+		end_date="${END_DATE}T00:00:00Z"
+	else
+		end_date="${END_DATE}"
+	fi
+	tmpfile="$(mktemp /tmp/${CONTAINER_NAME}_logs.$$.XXXX)"
+	addr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' iris_loki_1)
+	i=1
+	while [[ ! "${start_date}" > "${end_date}" ]]; do
+		next_start_date=$(add_days "${start_date}" 30)
+		to_date="$(subtract_second "${next_start_date}")"
+		(1>&2 date)
+		(1>&2 echo "logcli --quiet --addr=http://${addr}:3100 query ${QUERY} --from=${start_date} --to=${to_date} --limit 200000000 > ${tmpfile}.${i}")
+		logcli --quiet --addr="http://${addr}:3100" query "${QUERY}" --from="${start_date}" --to="${to_date}" --limit 200000000 > "${tmpfile}.${i}"
+		grep --color -i "${PATTERN}" "${tmpfile}.${i}" || :
 		echo
-                start_date="${next_start_date}"
-        done
-        rm -f "${tmpfile}".*
+		start_date="${next_start_date}"
+	done
+	rm -f "${tmpfile}".*
 }
 
 add_days() {
