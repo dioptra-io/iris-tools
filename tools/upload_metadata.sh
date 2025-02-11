6196ba4d49b857bede45432c64b004a63b8adfde
Elena Nardi <elena.nardi@lip6.fr>
2025-02-11 10:54:48 +0100

    Make the requested changes

--- tools/upload_metadata.sh
+++ tools/upload_metadata.sh
@@ -8,31 +8,24 @@ CONFIG_FILE="$(git rev-parse --show-toplevel)/conf/metadata_table.conf" # --conf
 FORCE=false # --force
 
 main() {
-	local output
-	local bq_public_dataset
-	local bq_private_dataset
-	local output_path
-	local flags
-	local irisctl_cmd
-	local bq_public_table
-	local bq_tmp_table
+	local datasets
 	local tmpfile
+	local bq_tmp_table
+	local bq_public_table
 
 	echo "sourcing ${CONFIG_FILE}"
 	# shellcheck disable=SC1090
 	source "${CONFIG_FILE}"
 
-	# Check if the public dataset exists.
-	# shellcheck disable=SC2153
-	bq_public_dataset="${BQ_PUBLIC_DATASET}"
-	echo verifying "${bq_public_dataset}"
-	check_dataset "${bq_public_dataset}"
-
-	# Check if the private dataset exists.
-	# shellcheck disable=SC2153
-	bq_private_dataset="${BQ_PRIVATE_DATASET}"
-	echo verifying "${bq_private_dataset}"
-	check_dataset "${bq_private_dataset}"
+	# check if the datasets exist
+	echo "checking datasets"
+	datasets=("${BQ_PUBLIC_DATASET}" "${BQ_PRIVATE_DATASET}")
+	for dataset in "${datasets[@]}"; do
+		if ! check_dataset_or_table "${dataset}"; then
+			echo "error: ${dataset} does not exist"
+			exit 1 
+		fi
+	done
 
 	if [[ -z "${IRIS_PASSWORD+x}" ]]; then
 		irisctl auth login
@@ -40,82 +33,136 @@ main() {
 		echo "irisctl will use IRIS_PASSWORD environment variable when invoked"
 	fi
 
-	# First, see if the measurement metadata file that includes all
-	# measurements needs to be created.  This file will be passed to
+	# First, create the measurement metadata file that includes all
+	# measurements if it does not esists. This file will be passed to
 	# `irisctl` in subsequent calls so speed up its execution.
-	if ! ${FORCE} && [[ -f "${MEAS_MD_ALL_JSON}" ]]; then
-		echo using existing "${MEAS_MD_ALL_JSON}"
-	else
-		echo creating "${MEAS_MD_ALL_JSON}"
-		if ! output="$(irisctl meas --all-users 2>&1 > /dev/null)"; then
-			echo "${output}"
-			exit 1
-		fi
-		output_path=$(echo "$output" | grep "saving in" | awk '{print $3}')
-		mv "${output_path}" "${MEAS_MD_ALL_JSON}"
+	if ! ensure_file_exists; then
+		echo "error: ${MEAS_MD_ALL_JSON} could not be created" 
+		exit 1
 	fi
+	# count measurements in MEAS_MD_ALL_JSON
 	echo "$(jq .count "${MEAS_MD_ALL_JSON}" | uniq) total measurements in ${MEAS_MD_ALL_JSON}"
 
 	# Next, select the measurements that we are interested in.
-	echo creating "${MEAS_MD_METADATA_TXT}"
-	# Add flags for filtering
+	echo "selecting measurements"
+    select_measurements
+	# count measurements in MEAS_MD_METADATA_TXT
+	echo "$(wc -l < "${MEAS_MD_METADATA_TXT}") selected measurements in ${MEAS_MD_METADATA_TXT}"
+
+	# Then, fetch metadata for selected measurements and save them to a temporary file.
+	tmpfile="$(mktemp /tmp/metadata.XXXX)"
+	echo "fetching metadata from ${MEAS_MD_METADATA_TXT} and saving them to ${tmpfile}"
+	fetch_metadata "${tmpfile}"
+
+	# Upload metadata to temporary metadata table.
+	# shellcheck disable=SC2153
+	bq_tmp_table="${BQ_PRIVATE_DATASET}"."${BQ_TMP_TABLE}"
+	echo "uploading metadata to ${bq_tmp_table} table"
+	upload_tmp_metadata "${tmpfile}" "${bq_tmp_table}"
+
+	# Upload metadata to BQ_PUBLIC_TABLE.
+	# shellcheck disable=SC2153
+	bq_public_table="${BQ_PUBLIC_DATASET}"."${BQ_PUBLIC_TABLE}"
+	echo "uploading metadata to ${bq_public_table}"
+	upload_public_metadata "${bq_public_table}" "${bq_tmp_table}"
+
+	# Delete temporary table after uploading.
+	bq rm -t -f --project_id="${GCP_PROJECT_ID}" "${BQ_PRIVATE_DATASET}"."${BQ_TMP_TABLE}"
+	echo "temporary table ${BQ_PRIVATE_DATASET}.${BQ_TMP_TABLE} deleted"
+}
+
+check_dataset_or_table() {
+	local resource="$1"
+
+	if  ! bq show --project_id="${GCP_PROJECT_ID}" "${resource}" > /dev/null 2>&1; then
+		return 1
+	fi
+}
+
+ensure_file_exists(){
+	local output
+	local output_path
+	
+	if ! ${FORCE} && [[ -f "${MEAS_MD_ALL_JSON}" ]]; then
+		echo "using existing ${MEAS_MD_ALL_JSON}"
+	else
+		echo "creating ${MEAS_MD_ALL_JSON}"
+		if ! output="$(irisctl meas --all-users 2>&1 > /dev/null)"; then
+			return 1
+		fi
+		output_path=$(echo "$output" | awk '/saving in/ {print $3}')
+		mv "${output_path}" "${MEAS_MD_ALL_JSON}"
+	fi
+}
+
+select_measurements(){
+	local flags
+	local irisctl_cmd
+
+	echo "creating ${MEAS_MD_METADATA_TXT}"
+	# add flags for filtering
  	flags=()
 	[ ${#MEAS_TAG[@]} -gt 0 ] && for tag in "${MEAS_TAG[@]}"; do flags+=("--tag" "$tag"); done
 	[ -n "${MEAS_BEFORE}" ] && flags+=("--before" "${MEAS_BEFORE}")
 	[ -n "${MEAS_AFTER}" ] && flags+=("--after" "${MEAS_AFTER}")
 	[ ${#MEAS_STATES[@]} -gt 0 ] && for state in "${MEAS_STATES[@]}"; do flags+=("--state" "$state"); done
-	# Debugging: Check if the flags array is empty
 	if [[ ${#flags[@]} -eq 0 ]]; then
-    echo "No flags provided. Running without flags."
+		irisctl_cmd=("irisctl" "list" "$MEAS_MD_ALL_JSON")  # no flags
 	else
-    echo "Flags provided: ${flags[*]}"
-	fi
-	# Construct the command
-	if [[ ${#flags[@]} -eq 0 ]]; then
-		irisctl_cmd=("irisctl" "list" "$MEAS_MD_ALL_JSON")  # No flags
-	else
-		irisctl_cmd=("irisctl" "list" "${flags[*]}" "${MEAS_MD_ALL_JSON}")  # With flags
+		irisctl_cmd=("irisctl" "list" "${flags[*]}" "${MEAS_MD_ALL_JSON}")  # with flags
 	fi	
 	echo "${irisctl_cmd[*]}"
 	eval "${irisctl_cmd[*]}" | awk '{print $1}' > "${MEAS_MD_METADATA_TXT}"
-	echo "$(wc -l < "${MEAS_MD_METADATA_TXT}") selected measurements in ${MEAS_MD_METADATA_TXT}"
+}
 
-	# Fetch metadata for selected measurements and save them to a temporary file.
-	tmpfile="$(mktemp /tmp/metadata.XXXX)"
-	echo fetching metadata from "${MEAS_MD_METADATA_TXT}" and saving them to "${tmpfile}"
+fetch_metadata(){
+	local tmpfile="$1"
+
 	while read -r uuid; do
 		irisctl list --bq --uuid "${uuid}" >> "${tmpfile}"
 	done < "${MEAS_MD_METADATA_TXT}"
- 
-    # Create the public table for inserting metadata if it doesn't exist.
-	bq_public_table="${BQ_PUBLIC_DATASET}"."${BQ_TABLE}"
-	if ! ${FORCE} && bq show --project_id="${GCP_PROJECT_ID}" "${bq_public_table}" > /dev/null 2>&1; then
-		echo "${bq_public_table} already exists"
-	else
-		# Create the table.
-		echo bq mk --project_id "${GCP_PROJECT_ID}" --schema "${SCHEMA_METADATA_JSON}" --table "${bq_public_table}"
-		bq mk --project_id "${GCP_PROJECT_ID}" --schema "${SCHEMA_METADATA_JSON}" --table "${bq_public_table}"
+}
 
-	fi
+upload_tmp_metadata(){
+	local tmpfile="$1"
+	local bq_tmp_table="$2"
 		
 	# Create the table for uploading temporary metadata.
-	# If it already exist, delete the existing table to prevent duplicate rows insertion
+	# If it already exist, delete the existing table to prevent duplicate rows insertion.
 	# shellcheck disable=SC2153
-	bq_tmp_table="${BQ_PRIVATE_DATASET}"."${BQ_TMP_TABLE}"
-	if ! ${FORCE} && bq show --project_id="${GCP_PROJECT_ID}" "${bq_tmp_table}" > /dev/null 2>&1; then
+	if check_dataset_or_table "${bq_tmp_table}"; then
 		echo "${bq_tmp_table} already exists"
 		bq rm -t -f --project_id="${GCP_PROJECT_ID}" "${bq_tmp_table}"
-		echo temporary table "${bq_tmp_table}" deleted
+		echo "temporary table ${bq_tmp_table} deleted"
 	fi
-	echo bq mk --project_id "${GCP_PROJECT_ID}" --schema "${SCHEMA_TMP_METADATA_JSON}" --table "${bq_tmp_table}"
-	bq mk --project_id "${GCP_PROJECT_ID}" --schema "${SCHEMA_TMP_METADATA_JSON}" --table "${bq_tmp_table}"
+	create_table "${SCHEMA_TMP_METADATA_JSON}" "${bq_tmp_table}"
 
-	# Load the metadata into the temporary metadata table
-	echo loading "${tmpfile}" into "${bq_tmp_table}"
-	bq load --project_id="${GCP_PROJECT_ID}" --source_format=CSV "${bq_tmp_table}" "${tmpfile}"
+	# Load the metadata into the temporary metadata table.
+	echo "loading ${tmpfile} into ${bq_tmp_table}"
+	bq load --replace --project_id="${GCP_PROJECT_ID}" --source_format=CSV "${bq_tmp_table}" "${tmpfile}"
+}
+
+create_table(){
+	local schema="$1"
+	local table="$2"
+
+	echo bq mk --project_id "${GCP_PROJECT_ID}" --schema "${schema}" --table "${table}"
+	bq mk --project_id "${GCP_PROJECT_ID}" --schema "${schema}" --table "${table}"
+}
+
+upload_public_metadata(){
+	local bq_public_table="$1"
+	local bq_tmp_table="$2"
+
+	# Create the public table for inserting metadata if it doesn't exist.
+	if check_dataset_or_table "${bq_public_table}"; then
+		echo "${bq_public_table} already exists"
+	else
+		create_table "${SCHEMA_METADATA_JSON}" "${bq_public_table}"
+	fi
 	
 	# Insert rows into metadata table.
-	# Construct the SQL query.
+	# construct the SQL query
 	SQL_QUERY="
 INSERT INTO \`${bq_public_table}\`
 SELECT
@@ -143,24 +190,9 @@ WHERE NOT EXISTS (
   WHERE tmp_metadata.id = metadata.id
 )
 "
-	# Execute the query using bq.
-	echo inserting metadata into "${bq_public_table}"
+	# execute the query using bq
+	echo "inserting metadata into ${bq_public_table}"
 	bq query --use_legacy_sql=false --project_id="${GCP_PROJECT_ID}" "${SQL_QUERY}"
-
-	# Delete temporary table after conversion.
-	bq rm -t -f --project_id="${GCP_PROJECT_ID}" "${bq_tmp_table}"
-	echo temporary table "${bq_tmp_table}" deleted
-
-}
-
-check_dataset() {
-	local dataset="$1"
-
-	# Check if dataset exists.
-	if  ! bq show --project_id="${GCP_PROJECT_ID}" "${dataset}" > /dev/null 2>&1; then
-		echo "error: ${dataset} does not exist"
-		exit 1
-	fi
 }
 
 main "$@"
